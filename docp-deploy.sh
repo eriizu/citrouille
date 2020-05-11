9e79e8bb54d2ef9cad3c0c5b37e6aa6ef44b131f
Élise Philippe <elise.ph@icloud.com>
2020-05-11 21:00:23 +0100

    removed the cookie from the deployement confirmation

--- docp-deploy.sh
+++ docp-deploy.sh
@@ -22,7 +22,6 @@ export $(cat conf.env | xargs)
 curl --request POST \
   --url $DEPLOY_CONFIRM_WEBHOOK \
   --header 'content-type: application/json' \
-  --cookie '__cfduid=dca695f0bb19f37c06f7235261c0eded71589193375; __cfruid=5b2e84f0b82352eac37a76a3907f5a72ca3d557b-1589193375' \
   --data '{
 	"content": "Je viens de finir un déployement !"
 }'
